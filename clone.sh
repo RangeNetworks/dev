bc0042a39ca21ccd038d1b7ffbedb96dcacc5a06
Michael Iedema <michael.iedema@rangenetworks.com>
2014-03-28 11:34:20 +0100

    - further fixups to deal with nested submodules correctly

--- clone.sh
+++ clone.sh
@@ -23,16 +23,18 @@ for component in $REPOS
 do
 	if [ ! -d $component ]; then
 		echo "# cloning $component"
-		sayAndDo git clone --recursive git@github.com:RangeNetworks/$component.git
+		sayAndDo git clone git@github.com:RangeNetworks/$component.git
 		cd $component
 		for remote in `git branch -r | grep -v master `
 		do
 			sayAndDo git checkout --track $remote
 		done
 		sayAndDo git checkout master
-		sayAndDo git submodule init
-		sayAndDo git submodule update --init --recursive
+		sayAndDo git submodule update --init --recursive --remote
+		# sayAndDo doesn't like this string at all, can't seem to get the quoting right
+		git submodule foreach 'git checkout `git config -f $toplevel/.gitmodules submodule.$name.branch`'
 		cd ..
 		echo
 	fi
 done
+
