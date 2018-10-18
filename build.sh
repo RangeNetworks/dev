68a2915ae45a03a34ff7dc72c04c67ce5b306af1
George Paitaris <wo@ipduh.com>
2018-10-18 16:55:23 +0300

    Adding Boost C++ dependency

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # See the COPYING file in the main directory for details.
@@ -115,6 +115,7 @@ installIfMissing libreadline-dev
 installIfMissing libncurses5
 installIfMissing libncurses5-dev
 installIfMissing pkg-config
+installIfMissing libboost-all-dev
 # libsqliteodbc deps
 installIfMissing cdbs
 installIfMissing libsqlite0-dev
